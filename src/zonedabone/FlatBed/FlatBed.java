2fe22a6d5c40b386a1ecf13ad468273d1bfd83a3
zonedabone <zonedabone@gmail.com>
2011-06-24 18:54:55 -0400

    Added environment checking. (Flattens ceiling in nether and nothing in skylands)

--- src/zonedabone/FlatBed/FlatBed.java
+++ src/zonedabone/FlatBed/FlatBed.java
@@ -3,6 +3,10 @@ package zonedabone.FlatBed;
 import java.io.File;
 import java.util.logging.Logger;
 
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.World.Environment;
+import org.bukkit.block.Block;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
@@ -31,6 +35,39 @@ public class FlatBed extends JavaPlugin {
 		chunks.load();pdf = this.getDescription();
 		log.info(pdf.getName() + " version " + pdf.getVersion() + " ENABLED");
 
+	}
+	
+	public void flatten(Chunk chunk){
+		String path = Integer.toString(chunk.getX())+'.'+Integer.toString(chunk.getZ());
+		if(!FlatBed.chunks.getBoolean(path, false)){
+			FlatBed.chunks.setProperty(path, true);
+			if(chunk.getWorld().getEnvironment()!=Environment.SKYLANDS){
+				for(int x = 0;x<16;x++){
+					for(int z = 0;z<16;z++){
+						chunk.getBlock(x, 0, z).setType(Material.BEDROCK);
+						for(int y = 1;y<6;y++){
+							Block block = chunk.getBlock(x, y, z);
+							if(block.getType()==Material.BEDROCK){
+								block.setType(Material.STONE);
+							}
+						}
+					}
+				}
+				if(chunk.getWorld().getEnvironment()==Environment.NETHER){
+					for(int x = 0;x<16;x++){
+						for(int z = 0;z<16;z++){
+							chunk.getBlock(x, 127, z).setType(Material.BEDROCK);
+							for(int y = 122;y<127;y++){
+								Block block = chunk.getBlock(x, y, z);
+								if(block.getType()==Material.BEDROCK){
+									block.setType(Material.STONE);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 }
